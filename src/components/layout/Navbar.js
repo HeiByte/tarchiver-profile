d27a35bdb701ae4d98b25a088d29c65f76014ed8
HeiByte <HeiByte@users.noreply.github.com>
2026-04-25 18:49:50 +0700

    tambah logo

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,5 +1,6 @@
-'use client';
+"use client";
 
+import Image from "next/image";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import MobileMenu from "../ui/MobileMenu";
@@ -15,12 +16,18 @@ const links = [
 export default function Navbar() {
   const pathname = usePathname();
 
- return (
+  return (
     <nav className="sticky top-0 z-50 bg-primary border-b border-slate-200">
       <div className="max-w-7xl mx-auto px-6 h-20 flex items-center justify-between">
         {/* Nama Aplikasi / Logo */}
-        <div className="text-xl font-bold text-white">
-          Tarchiver
+        <div className="flex justify-center items-center text-xl font-bold text-white">
+          <Image
+            src="/Logo.png"
+            alt="Tarchiver Logo"
+            width={140}
+            height={140}
+            priority
+          />
         </div>
 
         {/* Desktop Menu: (768px) ke atas */}
@@ -32,9 +39,10 @@ export default function Navbar() {
                 key={link.path}
                 href={link.path}
                 className={`text-primary px-4 py-2 rounded-lg text-sm font-medium transition-all duration-200 
-                  ${isActive 
-                    ? "bg-amber-100 text-amber-700" 
-                    : "text-white hover:bg-slate-50 hover:text-amber-600"
+                  ${
+                    isActive
+                      ? "bg-amber-100 text-amber-700"
+                      : "text-white hover:bg-slate-50 hover:text-amber-600"
                   }`}
               >
                 {link.name}
