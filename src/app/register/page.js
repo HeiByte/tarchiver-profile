ee48e710a27d3ae8488e8d14a97473224e8ae2df
ihsannuryansyah <ihsannuryansyah@gmail.com>
2026-04-30 09:05:51 +0700

    add page-login

---
+++ src/app/register/page.js
@@ -0,0 +1,10 @@
+import RegisterLayout from "@/components/auth/RegisterLayout";
+import RegisterForm from "@/components/auth/RegisterForm";
+
+export default function Register() {
+  return (
+    <RegisterLayout>
+      <RegisterForm />
+    </RegisterLayout>
+  );
+}
---
+++ src/components/auth/LoginLayout.js
@@ -0,0 +1,22 @@
+import Image from "next/image";
+import gambar7 from '@/assets/gambar7.jpg'
+
+export default function LoginLayout({ children }){
+    return(
+        <div className="min-h-screen flex items-center justify-center px-6 bg-white">
+            <div className="w-full max-w-6xl bg-white rounded-3xl shadow-lg overflow-hidden grid md:grid-cols-2">
+
+                {/* LEFT */}
+                <div className="p-10 md:p-16 flex flex-col items-center justify-center">
+                    {children}
+                </div>
+
+                {/* RIGHT */}
+                <div className="hidden md:flex items-center justify-center">
+                    <Image src={gambar7} alt="image-login"className="max-h-[80%] w-auto obejct-contain rounded-3xl" />
+                </div>
+            </div>
+        </div>
+
+    );
+}
