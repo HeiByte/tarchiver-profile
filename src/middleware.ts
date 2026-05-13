75147a927673a41c8739f72c1c71f98589fbd452
HeiByte <HeiByte@users.noreply.github.com>
2026-05-13 07:56:47 +0700

    fix bug landing page & edit style

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,10 +5,10 @@ export function middleware(request) {
   const timestamp = new Date().toLocaleTimeString();
   const session = request.cookies.get("auth_session");
 
-  console.info(`[${timestamp}] Middleware: Checking ${pathname}`);
+  // console.info(`[${timestamp}] Middleware: Checking ${pathname}`);
 
   if (!session) {
-    console.warn(`[${timestamp}] Middleware: No session found. Redirecting to /`);
+    // console.warn(`[${timestamp}] Middleware: No session found. Redirecting to /`);
     
     const url = request.nextUrl.clone();
     url.pathname = "/";
